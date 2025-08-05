6200574eb2dd59fc04386262ba417f0f31d8c812
Constantinos Makariou <cmakario@student.42heilbronn.de>
2025-08-05 17:26:49 +0200

    basic socket's class small refinements

--- Networking/basicSocket.hpp
+++ Networking/basicSocket.hpp
@@ -6,7 +6,7 @@
 /*   By: cmakario <cmakario@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 15:53:55 by cmakario          #+#    #+#             */
-/*   Updated: 2025/08/05 16:52:31 by cmakario         ###   ########.fr       */
+/*   Updated: 2025/08/05 17:25:36 by cmakario         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@
 #include <netinet/in.h>
 #include <iostream>
 
+// Inside this class we will create a basic socket that can be used for networking.
+// Specifically, it will handle the creation of a socket, binding it to an address, and testing the connection.
 class basicSocket {
 private:
 		struct sockaddr_in	address;
@@ -26,7 +28,7 @@ private:
 public:
 		// Constructor
 		basicSocket(int domain, int service, int protocol, int port, u_long interface);
-		// Test Connection
+		// Test socket creation and bind
 		void testConnection(int testConnectedObject) const;
 		// Getters
 		struct sockaddr_in getAddress() const;
