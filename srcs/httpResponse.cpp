cea2f4de9d91c76c732b51fd7545c441368f79bb
Constantinos Makariou <cmakario@student.42heilbronn.de>
2025-08-14 10:28:54 +0200

    really basic response build and build of Headers

--- srcs/httpResponse.cpp
+++ srcs/httpResponse.cpp
@@ -1,5 +1,48 @@
 #include "../inc/httpResponse.hpp"
 
+// ================== Helper Methods ==================
+void httpResponse::buildHeadersOnce()
+{
+	if (headersBuilt)
+	{
+		return;
+	}
+
+	std::ostringstream ss;
+	ss << "HTTP/1.1 " << statusCode << "\r\n";
+
+	// If file streaming: Content-Length is fileSize, else it's body.size()
+	if (useFile)
+	{
+		ss << "Content-Type: " << contentType << "\r\n";
+		ss << "Content-Length: " << static_cast<unsigned long long>(fileSize) << "\r\n";
+	}
+	else
+	{
+		ss << "Content-Type: " << contentType << "\r\n";
+		ss << "Content-Length: " << body.size() << "\r\n";
+	}
+
+	ss << "Connection: close\r\n";
+
+	// Add extra headers if any (avoid duplicates)
+	for (std::map<std::string,std::string>::const_iterator it = headers.begin(); it != headers.end(); ++it)
+	{
+		if (it->first != "Content-Type" && it->first != "Content-Length" && it->first != "Connection")
+		{
+			ss << it->first << ": " << it->second << "\r\n";
+		}
+	}
+
+	ss << "\r\n";
+	headerBytes = ss.str();
+	headerOffset = 0;
+	headersBuilt = true;
+}
+
+
+
+
 // ================== Costructors/Destructor ==================
 httpResponse::httpResponse()
 : statusCode("200 OK"),
@@ -59,45 +102,34 @@ void httpResponse::addHeader(const std::string &key, const std::string &value)
 }
 
 
+// ================== Build full string response ==================
 
-
-
-std::string httpResponse::buildResponse(const HttpRequest& request) const
+std::string httpResponse::buildResponse() const
 {
-    std::string status;
-    std::string content_type;
-    std::string body;
+	std::ostringstream ss;
+	ss << "HTTP/1.1 " << statusCode << "\r\n";
 
-    if(request.getPath() == "/")
-    {
-        status = "HTTP/1.1 200 OK";
-        content_type = "text/html; charset=utf-8";
-        body = request.readFile("./www/index2.html");
-    }
-    else if (request.getPath() == "/assets/images") // Fixed 'path' to 'request.getPath()'
-    {
-        status = "HTTP/1.1 200 OK";
-        content_type = "image/svg+xml";
-        body = request.readFile("./www/assets/images/logo.svg");
-    }
-    else
-    {
-        status = "HTTP/1.1 404 Not Found";
-        content_type = "text/html; charset=utf-8";
-        body = request.readFile("./www/Error/404.html");
-    }
+	// base headers
+	ss << "Content-Type: " << contentType << "\r\n";
+	ss << "Content-Length: " << body.size() << "\r\n";
+	ss << "Connection: close\r\n";
 
-    //Assemble the complete HTTP response
-    std::ostringstream response;
-    response << status << "\r\n";
-    response << "Content-Type: " << content_type << "\r\n";
-    response << "Content-Length: " << body.length() << "\r\n";
-    response << "\r\n";
-    response << body;
+	// extra headers
+	for (std::map<std::string,std::string>::const_iterator it = headers.begin(); it != headers.end(); ++it)
+	{
+		if (it->first != "Content-Type" && it->first != "Content-Length" && it->first != "Connection")
+		{
+			ss << it->first << ": " << it->second << "\r\n";
+		}
+	}
 
-    // std::cout << "---> HTTP Response:\n" << response.str() << std::endl; // print the complete HTTP response
-    return response.str();
+	ss << "\r\n";
+	ss << body;
+	return ss.str();
 }
+
+
+
 
 
 bool httpResponse::isDone() const
